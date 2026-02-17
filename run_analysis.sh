ca4f0b74413e924395798a013d5e1cc661c5c366
Francesco Mazzaschi <fmazzasc@alipap1.cern.ch>
2026-02-17 11:02:23 +0100

    update

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,12 +1,8 @@
-python3 analyse.py
-python3 analyse.py --me
-python3 analyse.py --mc
-python3 analyse.py --mc --me
-python3 ratio.py
-python3 ratio.py --mc
-python3 analyse.py --bantib
-python3 analyse.py --bantib --me
-python3 analyse.py --bantib --mc
-python3 analyse.py --bantib --mc --me
-python3 ratio.py --bantib
-python3 ratio.py --bantib --mc
+python3 analyse.py --output_dir "results_recal" --mom_recal
+python3 analyse.py --me --output_dir "results_recal" --mom_recal
+python3 ratio.py --output_dir "results_recal"
+
+
+python3 analyse.py --output_dir "results_norecal"
+python3 analyse.py --me --output_dir "results_norecal"
+python3 ratio.py --output_dir "results_norecal"
